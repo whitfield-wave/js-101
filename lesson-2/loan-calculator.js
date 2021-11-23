fac2c61e2969492131209fb85c3548d8c1ed4751
Anthony Whitfield <anthonywhitfield5027@yahoo.com>
2021-11-22 16:06:42 -0800

    adjusted for readability

--- lesson-2/loan-calculator.js
+++ lesson-2/loan-calculator.js
@@ -22,6 +22,7 @@ prompt('Welcome! This calculator calculates monthly payments and total cost of l
 do {
   prompt('How much in USD are you borrowing?');
   let loan = readLine.question();
+
   while (isInvalidNumber(loan)) {
     prompt('Please enter a valid positive number:');
     loan = readLine.question();
@@ -30,6 +31,7 @@ do {
 
   prompt('What is your APR percentage? (For example: 5%)');
   let aPR = readLine.question();
+
   while (isInvalidNumber(aPR)) {
     prompt('Please enter a valid positive number:');
     aPR = readLine.question();
@@ -39,24 +41,31 @@ do {
   prompt('What is the duration of your loan? (Please enter whole years and then remaining months.)');
   prompt('Years: ');
   let years = readLine.question();
+
   while (isInvalidNumber(years)) {
     prompt('Please enter a valid positive number:');
     years = readLine.question();
   }
+
   prompt('Months: ');
   let months = readLine.question();
+
   while (isInvalidNumber(months)) {
     prompt('Please enter a valid positive number:');
     months = readLine.question();
   }
+
   let durationMonths = (Number(years) * 12) + Number(months);
 
   let monthlyPayment = calcMonthly(loan, interest, durationMonths);
+
   let total = (monthlyPayment * durationMonths);
 
   prompt(`Your payment will be $${monthlyPayment.toFixed(2)}/month for a total of $${total.toFixed(2)} at the end of ${durationMonths.toFixed(0)} months.\n`);
+
   prompt('Would you like to calculate another loan? (y/n)');
   let answer = readLine.question();
+
   repeat = answer.startsWith('y');
 
   if (repeat) console.log('\n\n');
