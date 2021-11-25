e36defa1342ff7f03b64f1ddde3fe338afc4a8a1
Anthony Whitfield <anthonywhitfield5027@yahoo.com>
2021-11-25 12:02:26 -0800

    rock paper scissors liz spock final. added more functions and "best of" feature.;

--- lesson-2/rpsls.js
+++ lesson-2/rpsls.js
@@ -1,11 +1,10 @@
 const readline = require('readline-sync');
 const VALID_CHOICES = ['rock', 'paper', 'scissors','lizard','spock'];
 
+
 function prompt(message) {
   console.log(`=> ${message}`);
 }
-
-
 
 function isPlayerWinner(choice, computerChoice) {
   return ((choice === 'rock' && (computerChoice === 'scissors' || computerChoice === 'lizard')) ||
@@ -27,6 +26,47 @@ function displayWinner(choice, computerChoice) {
   }
 }
 
+function keepScore(scores, choice, computerChoice) {
+  if (isPlayerWinner(choice, computerChoice)) {
+    scores.player += 1;
+  } else {
+    scores.computer += 1;
+  }
+}
+
+function displayScoreToWin(scoreToWin) {
+  prompt(`The score to win is ${scoreToWin}.`);
+}
+
+function displayScore(scores) {
+  console.log('\n');
+  prompt(`The scores are Player: ${scores.player}     Computer: ${scores.computer}\n`);
+}
+
+function getScoreToWin() {
+  prompt('What score would you like to play to? Please pick a number from 1 to 5.');
+  let answer = readline.question();
+  while (Number.isNaN(Number(answer)) ||
+    (Number(answer) < 1 || Number(answer) > 5)) {
+    prompt('Please pick a number from 1 to 5.');
+    answer = readline.question();
+  }
+  return Number(answer);
+}
+
+function getYesOrNo() {
+  let input = readline.question().toLowerCase();
+  while (input[0] !== 'n' && input[0] !== 'y') {
+    prompt('Please enter "y" or "n".');
+    input = readline.question().toLowerCase();
+  }
+  return input;
+}
+
+let currentScore = {player: 0, computer: 0};
+
+let scoreToWin = getScoreToWin();
+
 while (true) {
   prompt(`Choose one: ${VALID_CHOICES.join(', ')}:`);
   let choice = readline.question();
@@ -40,13 +80,31 @@ while (true) {
   let computerChoice = VALID_CHOICES[randomIndex];
 
   displayWinner(choice, computerChoice);
-
-  prompt('Do you want to play again (y/n?)');
-  let answer = readline.question().toLowerCase();
-  while (answer[0] !== 'n' && answer[0] !== 'y') {
-    prompt('Please enter "y" or "n".');
-    answer = readline.question().toLowerCase();
+  if (choice !== computerChoice) {
+    keepScore(currentScore, choice, computerChoice);
   }
-  console.clear();
-  if (answer[0] !== 'y') break;
+  displayScore(currentScore);
+  displayScoreToWin(scoreToWin);
+
+  if (currentScore.player === Number(scoreToWin) ||
+      currentScore.computer === Number(scoreToWin)) {
+    prompt(currentScore.player > currentScore.computer ? 'The player wins the game!\n' : 'The computer wins the game!\n');
+
+    // resetting the scores.
+    currentScore.player = 0;
+    currentScore.computer = 0;
+
+    prompt('Do you want to play again (y/n?)');
+    let answer = getYesOrNo();
+    if (answer[0] !== 'y') {
+      console.clear();
+      break;
+    }
+
+    prompt('Do you want to change the winning score (y/n)?');
+    let scoreChange = getYesOrNo();
+    if (scoreChange[0] === 'y') getScoreToWin();
+
+    console.clear();
+  }
 }
